9c3ac1d6a58fd33f75a33db81aa34d64ba81a654
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-16 11:50:02 -0700

    Added randomization on load and next will be a button for random

--- iPhone Images/Controller/ViewController.m
+++ iPhone Images/Controller/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *iPhoneImageView;
+@property (nonatomic, strong) NSArray *urlArray;
 
 @end
 
@@ -19,7 +20,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSURL *url = [NSURL URLWithString:@"http://i.imgur.com/bktnImE.png"]; // 1
+    self.urlArray = @[@"http://imgur.com/bktnImE.png", @"http://imgur.com/zdwdenZ.png", @"http://imgur.com/CoQ8aNl.png", @"http://imgur.com/2vQtZBb.png", @"http://imgur.com/y9MIaCS.png"];
+    NSURL *url = [NSURL URLWithString:[self randomizeUrl]];
+    //NSURL *url = [NSURL URLWithString:@"http://i.imgur.com/bktnImE.png"]; // 1
     
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 2
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 3
@@ -52,6 +55,13 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSString*) randomizeUrl{
+    
+    int index = arc4random_uniform(4)-1;
+    NSString *string = [self.urlArray objectAtIndex:index];
+    return string;
 }
 
 
