e13374c2435caea006b32178477b6f9e8ac6c5a1
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-16 11:15:17 -0700

    Initial Commit

---
+++ iPhone Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhone Images
+//
+//  Created by Jamie on 2018-08-16.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhone Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhone Images
+//
+//  Created by Jamie on 2018-08-16.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
